ba4ec5562c700f911c3419e322d08eef2639f8d5
MoRsHeD <mdmorshed0187@gmail.com>
2026-07-04 15:49:23 +0600

    solved language dropdown show related issue in small device and profile photo index issue

--- components/Profile/ProfilePhotoUpload.tsx
+++ components/Profile/ProfilePhotoUpload.tsx
@@ -76,7 +76,7 @@ export default function ProfilePhotoUpload({ currentPhoto }: IProps) {
   };
 
   return (
-    <div className="relative z-999">
+    <div className="relative">
       <motion.div
         className="relative w-32 h-32 rounded-full overflow-hidden border-4 border-white shadow-xl"
         whileHover={{
